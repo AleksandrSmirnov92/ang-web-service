96d615fc272f6d8ff31efeec72ebc042bf7b384e
Aleksandr <ryan00@mail.ru>
2025-05-16 12:23:51 +0300

    add

--- backend/internal/config/config.go
+++
@@ -1,4 +0,0 @@
-package Config
-type Config struct {
-   Env string `yaml:"env" env:"ENV" env-default:"local"`
-}
--- backend/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func main() {
- // TODO: init config
- // TODO: init logger:slog
- // TODO: init storage
- // TODO: init router
- // TODO: run server 
-}
